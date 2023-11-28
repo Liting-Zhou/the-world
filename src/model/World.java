210bddf9f6e32eca8539156851b595c6e236c5d0
Liting-Zhou <143493320+Liting-Zhou@users.noreply.github.com>
2023-11-27 18:37:52 -0800

    add ReadOnlyWorld interface

--- src/model/World.java
+++ src/model/World.java
@@ -9,13 +9,6 @@ import java.util.List;
 public interface World extends ReadOnlyWorld{
 
   /**
-   * Gets the map of the mansion.
-   *
-   * @return the map
-   */
-  BufferedImage getMap();
-
-  /**
    * Adds human-controlled player to the game.
    *
    * @param playerName         The name of the player to add.
@@ -34,69 +27,12 @@ public interface World extends ReadOnlyWorld{
   void addComputerPlayer(String playerName, int startingRoomNumber, int maxNumOfWeapons);
 
   /**
-   * Gets the target.
-   *
-   * @return the target
-   */
-  Target getTarget();
-
-
-  /**
-   * Gets the current player.
-   *
-   * @return the current player
-   */
-  Player getCurrentPlayer();
-
-  /**
-   * Gets the list of players in the world.
-   *
-   * @return The list of players.
-   */
-  List<Player> getListOfPlayers();
-
-  /**
-   * Gets the list of rooms in the world.
-   *
-   * @return The list of rooms.
-   */
-  List<Room> getListOfRooms();
-
-  /**
-   * Gets the maximum number of turns can play.
-   *
-   * @return the maximum number of turns
-   */
-  int getMaxNumOfTurns();
-
-  /**
    * Sets the maximum number of turns.
    *
    * @param maxNumOfTurns the maximum number of turns
    */
   void setMaxNumOfTurns(int maxNumOfTurns);
 
-  /**
-   * Gets number of turns played.
-   *
-   * @return the number of turns played
-   */
-  int getNumOfTurnsPlayed();
-
-
-  /**
-   * When the health of target is less or equal to zero, game is over.
-   *
-   * @return the true if game over
-   */
-  Boolean isGameOver();
-
-  /**
-   * Gets the winner when game is over.
-   *
-   * @return the winner
-   */
-  Player getWinner();
 
   /**
    * Plays the next turn, in each turn, target moves and then one player moves.
@@ -142,24 +78,4 @@ public interface World extends ReadOnlyWorld{
    * @param y the y coordinate of the room
    */
   void moveToRoom(int x, int y);
-
-  /**
-   * Displays information about the target.
-   */
-  void displayTargetInformation();
-
-  /**
-   * Displays the list of rooms in the world.
-   */
-  void displayListOfRooms();
-
-  /**
-   * Displays information about the specified player.
-   */
-  void displayPlayerInformation();
-
-  /**
-   * Displays information about the target and players.
-   */
-  void displayRoomInformation();
 }
